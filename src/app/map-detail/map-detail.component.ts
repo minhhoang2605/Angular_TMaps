504bbd13599039ff7f69d542b45c63c26d71eb77
minhhoang2605 <minhhoang2605@gmail.com>
2019-08-30 10:49:53 +1000

    fix minor bug where pin and timer in map overlaping

--- src/app/map-detail/map-detail.component.ts
+++ src/app/map-detail/map-detail.component.ts
@@ -26,14 +26,14 @@ export class MapDetailComponent implements OnInit, DoCheck {
 
   ngOnInit() {
     this.getMap();
-    this.setClock();
+    this.setTimer();
   }
 
   ngDoCheck() {
     const newMapId = +this.route.snapshot.paramMap.get('id');
     if (this.map.id !== newMapId) {
       this.getMap();
-      this.setClock();
+      this.setTimer();
     }
   }
 
@@ -58,7 +58,7 @@ export class MapDetailComponent implements OnInit, DoCheck {
   }
 
   renderPins(): void {
-    for(let pin of this.pins) {
+    for (let pin of this.pins) {
       const img = document.createElement('img');
       const myMap = document.getElementById('map-container');
       img.setAttribute('src', '../assets/Map-Marker.png');
@@ -74,24 +74,32 @@ export class MapDetailComponent implements OnInit, DoCheck {
   }
 
   clearPins(): void {
-    let temp_pins = document.getElementsByClassName('pin');
-    while (temp_pins[0]) {
-      temp_pins[0].parentNode.removeChild(temp_pins[0]);
+    let tempPins = document.getElementsByClassName('pin');
+    while (tempPins[0]) {
+      tempPins[0].parentNode.removeChild(tempPins[0]);
     }
   }
 
-  setClock(): void {
+  setTimer(): void {
     this.time = 3000;
+    if (document.getElementById('timer') != null) {
+      document.getElementById('timer').innerHTML = '50:00';
+    }
     clearInterval(this.interval);
   }
 
-  runClock(): void {
+  runTimer(): void {
+    this.setTimer();
     this.interval = setInterval(() => {
       this.time--;
       const minutes = Math.floor(this.time / 60);
       const seconds = Math.floor(this.time % 60);
-      document.getElementById('timer').innerHTML
-          = minutes + ':' + seconds;
+      if (document.getElementById('timer') != null) {
+        document.getElementById('timer').innerHTML = minutes + ':' + seconds;
+      } else {
+        this.setTimer();
+        return;
+      }
       if (this.time === 0) {
         document.getElementById('timer').innerHTML = 'EXTRACTED';
         clearInterval(this.interval);
